7c4c5084b6257ec6820f61ebfbaca443a7f9e553
Anshita Vishwakarma <anshitavishwakarma098@gmail.com>
2019-12-27 03:02:00 +0530

    Add files via upload

---
+++ wicket-spring-hibernate-master/src/main/java/com/github/anirudhvarma12/whs/HomePage.java
@@ -0,0 +1,28 @@
+package com.github.anirudhvarma12.whs;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+public class HomePage extends WebPage {
+
+    @SpringBean
+    public SpringInterface inteface;
+
+    private static final long serialVersionUID = 1L;
+
+    private static Logger LOG = Logger.getLogger(HomePage.class);
+
+    public HomePage(final PageParameters parameters) {
+        super(parameters);
+        LOG.debug("Implmentation says " + inteface.getlist());
+       
+        // TODO Add your page's components here
+
+    }
+
+}
